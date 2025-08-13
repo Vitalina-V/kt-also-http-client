9e3321d7c4c2a7cd09f4db254a23dd0cc1c4096f
Volkivska <vitalina.v@air-dr.com>
2025-08-13 15:54:49 +0300

    processRequest impl

--- src/main/kotlin/mate/academy/HttpRequestProcessor.kt
+++ src/main/kotlin/mate/academy/HttpRequestProcessor.kt
@@ -1,7 +1,18 @@
 package mate.academy
 
+const val STATUS_OK = 200
 class HttpRequestProcessor(private val client: HttpClient) {
     fun processRequest(url: String): ResponseData {
-        // TODO: implement
+        val request = client.sendRequest(url)
+            .also {
+                println("Response Status: ${it.statusCode} - ${it.statusText}")
+                println("Response Content: ${it.content}")
+            }
+
+        return if (request.statusCode == STATUS_OK) {
+            ResponseData("Success", request.content.also { println("Processing content: $it") })
+        } else {
+            ResponseData("Failure", "Request failed with status: ${request.statusCode}")
+        }
     }
 }
